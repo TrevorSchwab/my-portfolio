7bdd4e52f544a18628dd56874acd436a43bdcdf4
Trevor Schwab <trevschwab@gmail.com>
2020-12-08 18:16:44 -0600

    Markup and styling for <Home />

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
 export default function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <main>
+      <div className="w-full h-full">
+        <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+          <h1 className="text-6xl text-grey-400 font-bold cursive leading-none lg:leading-snug home-name">
+            Hello, I'm Trevor.
+          </h1>
+        </section>
+      </div>
+    </main>
+  );
 }
